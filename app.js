3dfe01f225e12ed18cdc7ae6f135465bc020a77a
unknown <22220252@KTNG.AD>
2023-05-30 10:30:11 +0900

    백업

--- app.js
+++ app.js
@@ -30,42 +30,37 @@ function handleDisconnect() {
 
 handleDisconnect();
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 /**
  * 조회
  * @param
  * @return
  */
-app.get("/paget3l4/select-hjboard", (req, res) => {
+app.get("/paget3l4/select-njboard", (req, res) => {
   var param = { SEQ: req.query.SEQ, COMPLETE_YN: req.query.COMPLETE_YN };
   var format = { language: "sql", indent: "  " };
-  var query = mybatisMapper.getStatement("paget3l4", "selectHjBoard", param, format);
-  
- 
+  var query = mybatisMapper.getStatement("paget3l4", "selectNjBoard", param, format);
   dbconn.query(query, (error, rows) => {
     if (error) throw error;
     res.send(rows);
   });
 });
 
-
 /**
  * 입력/수정/삭제(N건)
  * @param
  * @return
  */
-app.post("/paget3l4/save-hjboard", function (req, res) {
-  console.log("param:", req.body.modifiedRows);
+app.post("/paget3l4/save-njboard", function (req, res) {
+  // console.log("param:", req.body.modifiedRows);
   /*  
     modifiedRows: {
-        createdRows: [ ...  ],
-        updatedRows: [ ...  ],
-        deletedRows: [ ...  ]
+        createdRows: [ {...},{...} ...  ],
+        updatedRows: [ {...},{...} ...  ],
+        deletedRows: [ {...},{...} ...  ]
     }
   */
   let sqlErrMsg = [];
@@ -73,7 +68,7 @@ app.post("/paget3l4/save-hjboard", function (req, res) {
   // 삭제
   req.body.modifiedRows.deletedRows?.forEach(function (row) {
     let SEQ = row.SEQ;
-    let sql = `DELETE FROM HJ_BOARD WHERE BOARD_ID = 'T3L4' AND SEQ = ${SEQ}`;
+    let sql = `DELETE FROM NJ_BOARD WHERE BOARD_ID = 'T3L4' AND SEQ = ${SEQ}`;
     dbconn.query(sql, function (err, result) {
       if (err) {
         sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
@@ -86,8 +81,8 @@ app.post("/paget3l4/save-hjboard", function (req, res) {
     let TITLE = row.TITLE;
     let CONTENT = row.CONTENT;
     let USER_ID = row.USER_ID;
-	let IMG_URL = row.IMG_URL;
-    let sql = `INSERT INTO HJ_BOARD(BOARD_ID, TITLE, CONTENT, USER_ID, CREATE_DT, IMG_URL) VALUES('T3L4', '${TITLE}', '${CONTENT}', 'njlee', ${"NOW()"}, '${IMG_URL}')`;
+    let IMG_URL = row.IMG_URL;
+    let sql = `INSERT INTO NJ_BOARD(BOARD_ID, TITLE, CONTENT, USER_ID, CREATE_DATE, IMG_URL) VALUES('T3L4', '${TITLE}', '${CONTENT}', 'njlee', ${"NOW()"}, '${IMG_URL}')`;
     dbconn.query(sql, function (err, result) {
       if (err) {
         sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
@@ -101,8 +96,41 @@ app.post("/paget3l4/save-hjboard", function (req, res) {
     let TITLE = row.TITLE;
     let CONTENT = row.CONTENT;
     let USER_ID = row.USER_ID;
-	let IMG_URL = row.IMG_URL;
-    let sql = `UPDATE HJ_BOARD SET TITLE = '${TITLE}', CONTENT = '${CONTENT}' , USER_ID = 'njlee' , CREATE_DT = ${"NOW()"}, IMG_URL = '${IMG_URL}' WHERE SEQ = ${SEQ}`;
+    let IMG_URL = row.IMG_URL;
+    let sql = `UPDATE NJ_BOARD SET TITLE = '${TITLE}', CONTENT = '${CONTENT}' , USER_ID = 'njlee' , CREATE_DATE = ${"NOW()"}, IMG_URL = '${IMG_URL}' WHERE SEQ = ${SEQ}`;
+    dbconn.query(sql, function (err, result) {
+      if (err) {
+        sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
+      }
+    });
+  });
+  // 모든 DML 처리후, 결과리턴을 위한 dummy select ... 더 좋은 방법은 ?
+  dbconn.query(`SELECT 1`, (error, rows) => {
+    res.json(JSON.stringify({ resMsg: sqlErrMsg.length === 0 ? "저장성공" : "에러발생", sqlErrMsg }));
+  });
+});
+
+/**
+ * 완료 toggle
+ * @param
+ * @return
+ */
+app.post("/paget3l4/toggle-complete", function (req, res) {
+  //console.log("param:", req.body.modifiedRows);
+  /*  
+    modifiedRows: {
+        createdRows: [ ...  ],
+        updatedRows: [ ...  ],
+        deletedRows: [ ...  ]
+    }
+  */
+  let sqlErrMsg = [];
+
+  // 수정
+  req.body.modifiedRows.updatedRows?.forEach(function (row) {
+    let SEQ = row.SEQ;
+    let COMPLETE_DATE = row.COMPLETE_DATE;
+    let sql = `UPDATE NJ_BOARD SET COMPLETE_DATE = '${COMPLETE_DATE}'  WHERE SEQ = ${SEQ}`;
     dbconn.query(sql, function (err, result) {
       if (err) {
         sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
@@ -117,26 +145,41 @@ app.post("/paget3l4/save-hjboard", function (req, res) {
 });
 
 /**
- * 완료 toggle
+ * 댓글조회
  * @param
  * @return
  */
-app.post("/paget3l4/toggle-complete", function (req, res) {
-  console.log("param:", req.body.modifiedRows);
+app.get("/paget3l4/select-comment", (req, res) => {
+  var param = { PARENT_SEQ: req.query.PARENT_SEQ };
+  var format = { language: "sql", indent: "  " };
+  var query = mybatisMapper.getStatement("paget3l4", "selectComment", param, format);
+
+  dbconn.query(query, (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
+  });
+});
+
+/**
+ * 댓글저장
+ * @param
+ * @return
+ */
+app.post("/paget3l4/save-comment", function (req, res) {
+  // console.log("param:", req.body.modifiedRows);
   /*  
     modifiedRows: {
-        createdRows: [ ...  ],
-        updatedRows: [ ...  ],
-        deletedRows: [ ...  ]
+        createdRows: [ {...},{...} ...  ],
+        updatedRows: [ {...},{...} ...  ],
+        deletedRows: [ {...},{...} ...  ]
     }
   */
   let sqlErrMsg = [];
- 
-  // 수정
-  req.body.modifiedRows.updatedRows?.forEach(function (row) {
+
+  // 삭제
+  req.body.modifiedRows.deletedRows?.forEach(function (row) {
     let SEQ = row.SEQ;
-    let COMPLETE_DATE = row.COMPLETE_DATE;
-    let sql = `UPDATE HJ_BOARD SET COMPLETE_DATE = '${COMPLETE_DATE}'  WHERE SEQ = ${SEQ}`;
+    let sql = `DELETE FROM NJ_BOARD_COMMENT WHERE SEQ = ${SEQ}`;
     dbconn.query(sql, function (err, result) {
       if (err) {
         sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
@@ -144,12 +187,39 @@ app.post("/paget3l4/toggle-complete", function (req, res) {
     });
   });
 
+  // 입력
+  req.body.modifiedRows.createdRows?.forEach(function (row) {
+    let PARENT_SEQ = row.PARENT_SEQ;
+    let CONTENT = row.CONTENT;
+    let USER_ID = row.USER_ID;
+    let IMG_URL = row.IMG_URL;
+    let sql = `INSERT INTO NJ_BOARD_COMMENT(PARENT_SEQ, CONTENT, IMG_URL, CREATE_USER, CREATE_DATE) VALUES( ${PARENT_SEQ}, '${CONTENT}', '${IMG_URL}', 'njlee', ${"NOW()"})`;
+
+    dbconn.query(sql, function (err, result) {
+      if (err) {
+        sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
+      }
+    });
+  });
+
+  // 수정
+  req.body.modifiedRows.updatedRows?.forEach(function (row) {
+    let SEQ = row.SEQ;
+    let CONTENT = row.CONTENT;
+    let USER_ID = row.USER_ID;
+    let IMG_URL = row.IMG_URL;
+    let sql = `UPDATE NJ_BOARD_COMMENT SET CONTENT = '${CONTENT}', IMG_URL = '${IMG_URL}, CREATE_USER = 'njlee' , CREATE_DATE = ${"NOW()"}' WHERE SEQ = ${SEQ}`;
+    dbconn.query(sql, function (err, result) {
+      if (err) {
+        sqlErrMsg.push({ sqlMessage: err.sqlMessage, sql: err.sql });
+      }
+    });
+  });
   // 모든 DML 처리후, 결과리턴을 위한 dummy select ... 더 좋은 방법은 ?
   dbconn.query(`SELECT 1`, (error, rows) => {
     res.json(JSON.stringify({ resMsg: sqlErrMsg.length === 0 ? "저장성공" : "에러발생", sqlErrMsg }));
   });
 });
-
 
 const server = app.listen(port, "0.0.0.0", () => {
   var host = server.address().address; // typescript 에서 에러발생 주석
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 //const HOST_NAME = ZnCommon.getHostName();
 const HOST_NAME = "http://localhost:8080";
+let gParentSeq;
+const G_LOGIN_STATE = { isLogin: true, userId: "njlee", userName: "홍길동" };
 
 /**
  * todo 목록을 조회한다.
@@ -8,7 +10,7 @@ const HOST_NAME = "http://localhost:8080";
  */
 const fetchData = async (opt) => {
   let completeYN = opt === "TODO" ? "N" : "Y";
-  const url = `${HOST_NAME}/paget3l4/select-hjboard?SEQ=&COMPLETE_YN=${completeYN}`;
+  console.log("fetchData.url", url);
   const response = await fetch(url);
   const data = await response.json();
   return data;
@@ -16,13 +18,12 @@ const fetchData = async (opt) => {
 
 /**
  * todo 목록을 렌더링한다.
+ * @param {string} opt - TODO:할일, COMPLETE:완료
  * @param {object} todos - 할일목록
  */
-const renderData = (todos) => {
+const renderData = (opt, todos) => {
+  loadTemplatePage("#list-page");
   const divList = document.querySelector(".list-bg");
-  removeAllChildNodes(divList);
-  document.querySelector("#write-wrapper").style.display = "none";
-  document.querySelector("#detail-wrapper").style.display = "none";
 
   todos.map((todo) => {
     const divListItem = document.createElement("div");
@@ -34,6 +35,9 @@ const renderData = (todos) => {
     const spanEdit = document.createElement("span");
     const spanDelete = document.createElement("span");
     const pContent = document.createElement("p");
+    const spanCreateDate = document.createElement("span");
+    const spanCompleteDate = document.createElement("span");
+    const spanCreateUser = document.createElement("span");
 
     divListItem.setAttribute("class", "list-item");
     divListItem.setAttribute("data-seq", todo.SEQ);
@@ -42,7 +46,14 @@ const renderData = (todos) => {
     spanComplete.textContent = "✔️";
     spanEdit.textContent = "✏️";
     spanDelete.textContent = "🗑️";
+    spanDelete.setAttribute("data-comment_cnt", todo.COMMENT_CNT);
     pContent.textContent = todo.CONTENT;
+    spanCreateDate.textContent = todo.CREATE_DATE_SHORT;
+    spanCreateUser.textContent = "/" + todo.USER_NAME;
+
+    if (todo.COMMENT_CNT > 0) {
+      h4Title.textContent += ` [${todo.COMMENT_CNT}]`;
+    }
 
     divTitleLeft.appendChild(h4Title);
     divTitleRight.appendChild(spanComplete);
@@ -52,6 +63,12 @@ const renderData = (todos) => {
     divTitleContainer.appendChild(divTitleRight);
     divListItem.appendChild(divTitleContainer);
     divListItem.appendChild(pContent);
+    divListItem.appendChild(spanCreateDate);
+    if (todo.COMPLETE_DATE !== "N") {
+      spanCompleteDate.textContent = "/" + todo.COMPLETE_DATE.substring(2, 4) + "." + todo.COMPLETE_DATE.substring(4, 6) + "." + todo.COMPLETE_DATE.substring(6, 8);
+      divListItem.appendChild(spanCompleteDate);
+    }
+    divListItem.appendChild(spanCreateUser);
     divList.appendChild(divListItem);
 
     if (todo.COMPLETE_DATE !== "N") {
@@ -60,7 +77,8 @@ const renderData = (todos) => {
 
     // TITLE click event handler
     divTitleLeft.addEventListener("click", () => {
-      renderDetailPage({ SEQ: todo.SEQ, TITLE: todo.TITLE, CONTENT: todo.CONTENT, IMG_URL: todo.IMG_URL });
+      //renderDetailPage({ SEQ: todo.SEQ, TITLE: todo.TITLE, CONTENT: todo.CONTENT, IMG_URL: todo.IMG_URL });
+      renderDetailPage(todo);
     });
 
     // ✔️ click event handler
@@ -73,20 +91,49 @@ const renderData = (todos) => {
         e.target.parentElement.parentElement.parentElement.style.background = "rgb(255, 165, 0)";
         today = new Date().toISOString().substring(0, 10).replace(/-/g, "");
       }
+      if (!G_LOGIN_STATE.isLogin) {
+        //alert("로그인이 필요한 서비스 입니다.");
+        return;
+      }
+
+      // 로그인 일때만 DB저장
       toggleComplete({ SEQ: todo.SEQ, COMPLETE_DATE: today });
     });
 
     // ✏️ click event handler
     spanEdit.addEventListener("click", (event) => {
-      renderWritePage({ SEQ: todo.SEQ, TITLE: todo.TITLE, CONTENT: todo.CONTENT, IMG_URL: todo.IMG_URL });
+      if (!G_LOGIN_STATE.isLogin) {
+        alert("로그인이 필요한 서비스 입니다.");
+        return;
+      }
+      if (loginChk.LOGIN_USER_ID !== todo.USER_ID) {
+        alert("자신의 글만 수정가능 합니다.");
+        return;
+      }
+      renderWritePage({ SEQ: todo.SEQ, TITLE: todo.TITLE, CONTENT: todo.CONTENT, IMG_URL: todo.IMG_URL, USER_ID: todo.USER_ID });
     });
 
     // 🗑️ click event handler
-    spanDelete.addEventListener("click", (e) => {
-      if (!confirm("삭제하시겠습니까?")) return;
+    spanDelete.addEventListener("click", async (e) => {
       let seq = e.target.parentElement.parentElement.parentElement.dataset.seq;
+      let commentCnt = e.target.dataset.comment_cnt;
+      if (commentCnt > 0) {
+        alert("댓글이 있는글은 삭제할 수 없습니다.");
+        return;
+      }
+
+      if (!G_LOGIN_STATE.isLogin) {
+        alert("로그인이 필요한 서비스 입니다.");
+        return;
+      }
+
+      if (!confirm("삭제하시겠습니까?")) return;
       saveData([{ IUD_FLAG: "D", SEQ: seq }]);
     });
+
+    if (opt === "COMPLETE") {
+      divTitleContainer.removeChild(divTitleRight);
+    }
   });
 };
 
@@ -107,7 +154,7 @@ const saveData = async (objArr) => {
   //       updatedRows: [ {...},{...} ...  ],
   //       deletedRows: [ {...},{...} ...  ]
   // }
-  const res = await fetch(`${HOST_NAME}/paget3l4/save-hjboard`, {
+  const res = await fetch(`${HOST_NAME}/paget3l4/save-njboard`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -117,7 +164,7 @@ const saveData = async (objArr) => {
   });
 
   const resJSON = await res.json();
-  fetchData("TODO").then(renderData);
+  fetchData("TODO").then((json) => renderData("TODO", json));
 };
 
 /**
@@ -138,7 +185,145 @@ const toggleComplete = async (obj) => {
   });
 
   const resJSON = await res.json();
-  fetchData("TODO").then(renderData);
+};
+
+/**
+ * 댓글을 조회한다.
+ * @param {string} parentSeq - 게시글번호
+ * @returns {promise([JSON array])} data - 조회된 댓글목록
+ */
+const fetchComment = async (parentSeq) => {
+  const url = `${HOST_NAME}/paget3l4/select-comment?PARENT_SEQ=${parentSeq}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+/**
+ * 댓글 목록을 렌더링한다.
+ * @param {number} parentSeq - 게시글번호
+ * @param {object} comments - 댓글목록
+ */
+const renderComment = (comments) => {
+  const divCommentList = document.querySelector("#comment-list-wrapper");
+  removeAllChildNodes(divCommentList);
+
+  comments.map((comment) => {
+    const hr = document.createElement("hr");
+    const divCommentItem = document.createElement("div");
+    const divTitleContainer = document.createElement("div");
+    const divTitleLeft = document.createElement("div");
+    const divTitleRight = document.createElement("div");
+    const pContent = document.createElement("p");
+
+    const spanCreateUserName = document.createElement("span");
+    const spanCreateDate = document.createElement("span");
+    const spanComplete = document.createElement("span");
+    const spanDelete = document.createElement("span");
+
+    divTitleContainer.setAttribute("class", "detail-comment-title-container");
+    spanCreateUserName.textContent = comment.CREATE_USER_NAME;
+    spanCreateDate.textContent = comment.CREATE_DATE;
+    pContent.textContent = comment.CONTENT;
+    spanComplete.textContent = "✔️";
+    spanDelete.textContent = "🗑️";
+    spanDelete.setAttribute("data-seq", comment.SEQ);
+
+    divTitleLeft.appendChild(spanCreateUserName);
+    divTitleRight.appendChild(spanCreateDate);
+    divTitleRight.appendChild(spanComplete);
+    divTitleRight.appendChild(spanDelete);
+    divTitleContainer.appendChild(divTitleLeft);
+    divTitleContainer.appendChild(divTitleRight);
+
+    divCommentItem.appendChild(hr);
+    divCommentItem.appendChild(divTitleContainer);
+    divCommentItem.appendChild(pContent);
+    divCommentList.appendChild(divCommentItem);
+
+    if (comment.IMG_URL) {
+      const img = document.createElement("img");
+      img.src = comment.IMG_URL;
+      divCommentList.appendChild(img);
+    }
+
+    // ✔️ click event handler
+    spanComplete.addEventListener("click", (e) => {
+      let today;
+      if (e.target.parentElement.parentElement.parentElement.style.background === "rgb(255, 165, 0)") {
+        e.target.parentElement.parentElement.parentElement.style.background = "white";
+        today = "N";
+      } else {
+        e.target.parentElement.parentElement.parentElement.style.background = "rgb(255, 165, 0)";
+        today = new Date().toISOString().substring(0, 10).replace(/-/g, "");
+      }
+      //toggleComplete({ SEQ: todo.SEQ, COMPLETE_DATE: today });
+    });
+
+    // 🗑️ click event handler
+    spanDelete.addEventListener("click", (e) => {
+      if (!G_LOGIN_STATE.isLogin) {
+        alert("로그인이 필요한 서비스 입니다.");
+        return;
+      }
+
+      if (!confirm("삭제하시겠습니까?")) return;
+      let seq = e.target.dataset.seq;
+      saveComment([{ IUD_FLAG: "D", SEQ: seq }]);
+    });
+  });
+
+  const inputCONTENT = document.querySelector("#comment-CONTENT");
+  const inputIMG_URL = document.querySelector("#comment-IMG_URL");
+  inputCONTENT.value = "";
+  inputIMG_URL.value = "";
+
+  const btnCommentSave = document.querySelector("#btn-comment-save");
+  if (btnCommentSave.classList.contains("once1")) return; // 이벤트핸들러 중복등록 체크
+  btnCommentSave.classList.add("once1");
+  btnCommentSave.addEventListener("click", (e) => {
+    if (!G_LOGIN_STATE.isLogin) {
+      alert("로그인이 필요한 서비스 입니다.");
+      return;
+    }
+
+    if (inputCONTENT.value.trim() === "") {
+      alert("내용을 입력해 주세요.");
+      inputCONTENT.focus();
+      return;
+    }
+    saveComment([{ IUD_FLAG: "I", PARENT_SEQ: gParentSeq, CONTENT: inputCONTENT.value, IMG_URL: inputIMG_URL.value }]);
+  });
+};
+
+/**
+ * 댓글을 저장한다.
+ * @param {object[]} objArr - 저장할 todo object 배열: [{},{},{}...]
+ */
+const saveComment = async (objArr) => {
+  const createdRows = objArr.filter((v) => v.IUD_FLAG === "I");
+  const updatedRows = objArr.filter((v) => v.IUD_FLAG === "U");
+  const deletedRows = objArr.filter((v) => v.IUD_FLAG === "D");
+  const saveJSON = { modifiedRows: { createdRows, updatedRows, deletedRows } };
+
+  // save JSON 양식
+  //
+  // modifiedRows: {
+  //       createdRows: [ {...},{...} ...  ],
+  //       updatedRows: [ {...},{...} ...  ],
+  //       deletedRows: [ {...},{...} ...  ]
+  // }
+  const res = await fetch(`${HOST_NAME}/paget3l4/save-comment`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(saveJSON),
+  });
+
+  const resJSON = await res.json();
+  fetchComment(gParentSeq).then(renderComment);
 };
 
 function removeAllChildNodes(parent) {
@@ -148,51 +333,66 @@ function removeAllChildNodes(parent) {
 }
 
 function renderDetailPage(obj) {
-  const detailWrapper = document.querySelector("#detail-wrapper");
+  loadTemplatePage("#detail-page");
 
-  removeAllChildNodes(document.querySelector(".list-bg"));
-  document.querySelector("#write-wrapper").style.display = "none";
-  detailWrapper.style.display = "block";
-
-  document.querySelector("#detail-SEQ").textContent = obj.SEQ;
   document.querySelector("#detail-TITLE").textContent = obj.TITLE;
+  document.querySelector("#detail-USER_NAME").textContent = obj.USER_NAME;
+  document.querySelector("#detail-CREATE_DATE").textContent = obj.CREATE_DATE;
+  if (obj.COMPLETE_DATE !== "N") {
+    document.querySelector("#detail-CREATE_DATE").textContent += "(" + obj.COMPLETE_DATE.substring(2, 4) + "." + obj.COMPLETE_DATE.substring(4, 6) + "." + obj.COMPLETE_DATE.substring(6, 8) + ")";
+  }
+  //document.querySelector("#detail-COMPLETE_DATE").textContent = obj.COMPLETE_DATE;
   document.querySelector("#detail-CONTENT").textContent = obj.CONTENT;
+  const img = document.querySelector("#detail-IMG_URL");
 
-  if (detailWrapper.querySelector("img")) {
-    detailWrapper.removeChild(detailWrapper.querySelector("img"));
-  }
   if (obj.IMG_URL) {
-    const img = document.createElement("img");
+    img.style.display = "block";
     img.src = obj.IMG_URL;
-    detailWrapper.appendChild(img);
+  } else {
+    img.style.display = "none";
   }
+
+  gParentSeq = obj.SEQ;
+  fetchComment(gParentSeq).then(renderComment);
 }
 
 function renderWritePage(obj) {
-  removeAllChildNodes(document.querySelector(".list-bg"));
-  document.querySelector("#write-wrapper").style.display = "block";
+  loadTemplatePage("#write-page");
 
   const inputSEQ = document.querySelector("input[name='SEQ']");
   const inputTITLE = document.querySelector("input[name='TITLE']");
   const inputCONTENT = document.querySelector("input[name='CONTENT']");
   const inputIMG_URL = document.querySelector("input[name='IMG_URL']");
+  const inputUSER_ID = document.querySelector("input[name='USER_ID']");
 
   if (obj) {
     inputSEQ.value = obj.SEQ;
     inputTITLE.value = obj.TITLE;
     inputCONTENT.value = obj.CONTENT;
     inputIMG_URL.value = obj.IMG_URL;
+    inputUSER_ID.value = obj.USER_ID;
   } else {
     inputSEQ.value = "";
     inputTITLE.value = "";
     inputCONTENT.value = "";
     inputIMG_URL.value = "";
+    inputUSER_ID.value = "";
   }
 
   const btnSave = document.querySelector("#btn-save");
   if (btnSave.classList.contains("once1")) return; // 이벤트핸들러 중복등록 체크
   btnSave.classList.add("once1");
   btnSave.addEventListener("click", (e) => {
+    if (!G_LOGIN_STATE.isLogin) {
+      alert("로그인이 필요한 서비스 입니다.");
+      return;
+    }
+    let iudFlag = inputSEQ.value > 0 ? "U" : "I";
+
+    if (iudFlag === "U" && G_LOGIN_STATE.userId !== document.querySelector("input[name='USER_ID']").value) {
+      alert("자신의 글만 수정가능 합니다.");
+      return;
+    }
     if (inputTITLE.value.trim() === "") {
       alert("제목을 입력해 주세요.");
       inputTITLE.focus();
@@ -204,42 +404,169 @@ function renderWritePage(obj) {
       return;
     }
 
-    let iudFlag = inputSEQ.value > 0 ? "U" : "I";
-    console.log("iudFlag:", iudFlag);
     saveData([{ IUD_FLAG: iudFlag, SEQ: inputSEQ.value, TITLE: inputTITLE.value, CONTENT: inputCONTENT.value, IMG_URL: inputIMG_URL.value }]);
   });
 }
 
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+  options = {
+    path: "/",
+    // 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
+    ...options,
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function setDarkmode(mode) {
+  const body = document.querySelector("body");
+  const topDarkmode = document.querySelector("#top-darkmode");
+  if (mode === "dark" || mode == undefined) {
+    body.setAttribute("class", "dark-mode");
+    topDarkmode.innerHTML = "☀️";
+    setCookie("darkmode", "dark", { "max-age": 3600 });
+  } else {
+    body.removeAttribute("class");
+    topDarkmode.innerHTML = "🌙";
+    setCookie("darkmode", "light", { "max-age": 3600 });
+  }
+}
+
+function loadTemplatePage(pageId) {
+  var article = document.querySelector("article");
+  removeAllChildNodes(article);
+  var t = document.querySelector(pageId);
+  var clone = document.importNode(t.content, true);
+  article.appendChild(clone);
+}
+
+async function handleBtnLoginClick(e) {
+  G_LOGIN_STATE.isLogin = true;
+  G_LOGIN_STATE.userId = "njlee";
+  G_LOGIN_STATE.userName = "홍길동";
+  initPage();
+
+  // const { isLoginOK, LOGIN_USER_NAME } = await ZnCommon.isLogin();
+  // const inputUSER_ID = document.querySelector("input[name='USER_ID']");
+  // const inputUSER_PW = document.querySelector("input[name='USER_PW']");
+
+  // if (inputUSER_ID.value.trim() === "") {
+  //   alert("아이디를 입력해 주세요.");
+  //   inputUSER_ID.focus();
+  //   return;
+  // }
+  // if (inputUSER_PW.value.trim() === "") {
+  //   alert("패스워드를 입력해 주세요.");
+  //   inputUSER_PW.focus();
+  //   return;
+  // }
+  // if (!isLoginOK) {
+  //   const loginRet = await ZnCommon.loginPw(inputUSER_ID.value, CryptoJS.SHA256(inputUSER_PW.value).toString().toUpperCase()); // 로그인 진행
+  //   G_LOGIN_STATE.isLogin = loginRet.isLoginOK;
+  //   G_LOGIN_STATE.userId = loginRet.LOGIN_USER_ID;
+  //   G_LOGIN_STATE.userName = loginRet.LOGIN_USER_NAME;
+  //   if (loginRet.isLoginOK) {
+  //     initPage();
+  //   }
+  // }
+}
+
+async function handleLogout() {
+  if (!confirm("로그아웃 할까요?")) return;
+  //const ret = await ZnCommon.logout();
+  //if (JSON.parse(ret).RST_MSG === "저장성공") {
+  G_LOGIN_STATE.isLogin = false;
+  G_LOGIN_STATE.userId = "";
+  G_LOGIN_STATE.userName = "";
+  initPage();
+  //}
+}
+
+// 로그아웃, 로그인시 refresh
+async function initPage() {
+  const topBtnGoLogin = document.querySelector("#btn-go-login");
+  fetchData("TODO").then((json) => renderData("TODO", json));
+  if (G_LOGIN_STATE.isLogin) {
+    G_LOGIN_STATE.isLogin = true;
+    G_LOGIN_STATE.userId = "njlee";
+    G_LOGIN_STATE.userName = "홍길동";
+
+    document.getElementById("top-login-user-name").innerHTML = "홍길동";
+    topBtnGoLogin.textContent = "로그아웃";
+  } else {
+    G_LOGIN_STATE.isLogin = false;
+    G_LOGIN_STATE.userId = "";
+    G_LOGIN_STATE.userName = "";
+
+    document.getElementById("top-login-user-name").innerHTML = "";
+    topBtnGoLogin.textContent = "로그인";
+  }
+}
+
 function init() {
+  const body = document.querySelector("body");
   const topTodo = document.querySelector("#top-todo");
   const topComplete = document.querySelector("#top-complete");
   const topNew = document.querySelector("#top-new");
+  const topBtnGoLogin = document.querySelector("#btn-go-login");
   const topDarkmode = document.querySelector("#top-darkmode");
 
-  topTodo.addEventListener("click", (event) => {
-    fetchData("TODO").then(renderData);
+  topTodo.addEventListener("click", (e) => {
+    fetchData("TODO").then((json) => renderData("TODO", json));
   });
 
-  topComplete.addEventListener("click", (event) => {
-    fetchData("COMPLETE").then(renderData);
+  topComplete.addEventListener("click", (e) => {
+    fetchData("COMPLETE").then((json) => renderData("COMPLETE", json));
   });
 
-  topNew.addEventListener("click", (event) => {
+  topNew.addEventListener("click", (e) => {
     renderWritePage();
   });
 
-  topDarkmode.addEventListener("click", (event) => {
-    const body = document.querySelector("body");
-    if (body.getAttribute("class", "dark-mode") === "dark-mode") {
-      body.removeAttribute("class");
-      topDarkmode.innerHTML = "🌙";
-    } else {
-      body.setAttribute("class", "dark-mode");
-      topDarkmode.innerHTML = "☀️";
+  topBtnGoLogin.addEventListener("click", (e) => {
+    if (topBtnGoLogin.textContent === "로그아웃") {
+      handleLogout();
+      return;
     }
+
+    loadTemplatePage("#login-page");
+    const topBtnLogin = document.querySelector("#btn-login");
+    document.querySelector("input[name='USER_ID']").focus();
+
+    if (topBtnLogin.classList.contains("once1")) return; // 이벤트핸들러 중복등록 체크
+    topBtnLogin.classList.add("once1");
+
+    topBtnLogin.addEventListener("click", (e) => {
+      handleBtnLoginClick();
+    });
   });
 
-  topTodo.click();
+  topDarkmode.addEventListener("click", (e) => {
+    let mode = body.getAttribute("class", "dark-mode") === "dark-mode" ? "light" : "dark";
+    setDarkmode(mode);
+  });
+
+  setDarkmode(getCookie("darkmode"));
+  initPage();
 }
 
 init();
